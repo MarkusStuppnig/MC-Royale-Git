b66db48051284fe3313afe42387acfb19b1f006a
Stoffi05 <kekekake7@gmail.com>
2022-05-18 15:24:34 +0200

    heal

--- MC-Royale/src/health/PlayerItemConsumeListener.java
+++ MC-Royale/src/health/PlayerItemConsumeListener.java
@@ -1,11 +1,9 @@
 package health;
 
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
-import org.bukkit.inventory.ItemStack;
 
 import item.Colors;
 
@@ -17,9 +15,9 @@ public class PlayerItemConsumeListener implements Listener{
 		Player p = (Player) e.getPlayer();
 		
 		if(e.getItem().getItemMeta().getDisplayName().equals(Colors.green + Colors.bold + "Heal")) {
-			p.setFoodLevel(p.getFoodLevel() + 5);
+			p.setFoodLevel(p.getFoodLevel() + 1);
 			p.sendMessage("healed");
-			p.getInventory().removeItem(new ItemStack(Material.COOKED_CHICKEN, 1));
+			p.getInventory().removeItem(e.getItem());
 		}
 		e.setCancelled(true);
 	}
