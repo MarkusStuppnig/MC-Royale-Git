c3ed6e3463a40841fa29fe56adff4dae744024c7
MarkusStuppnig <mstuppnig@student.tgm.ac.at>
2022-05-19 21:41:21 +0200

    Doubled damage of all registered weapons ak47 - 8 pump - 10 sniper - 42

--- MC-Royale/src/weapon/stats/WeaponStats.java
+++ MC-Royale/src/weapon/stats/WeaponStats.java
@@ -14,14 +14,14 @@ public class WeaponStats {
 	public static final String sniper_display_name = Colors.dark_gray + Colors.bold + "Sniper";
 	
 	public static Weapon ak47() {
-		return new Weapon(Material.DIAMOND, ak47_display_name, 20, 2.3, 5F, 5F, 1, 1, 4);
+		return new Weapon(Material.DIAMOND, ak47_display_name, 20, 2.3, 5F, 5F, 1, 1, 8);
 	}
 	
 	public static Weapon pump() {
-		return new Weapon(Material.EMERALD, pump_display_name, 6, 3.0, 1.5F, 50F, 200, 10, 5);
+		return new Weapon(Material.EMERALD, pump_display_name, 6, 3.0, 1.5F, 50F, 200, 10, 10);
 	}
 	
 	public static Weapon sniper() {
-		return new Weapon(Material.IRON_INGOT, sniper_display_name, 6, 5.0, 12F, 0.02F, 1, 20, 22);
+		return new Weapon(Material.IRON_INGOT, sniper_display_name, 6, 5.0, 12F, 0.02F, 1, 20, 42);
 	}
 }
