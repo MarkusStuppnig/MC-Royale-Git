bacbe4c0e36fea59db34ab30a9f30c6559f50621
Texotek <texotek17@gmail.com>
2022-05-26 12:14:28 +0200

    Villager doenst tranform anymore

--- MC-Royale/src/listeners/EntityTransformListener.java
+++ MC-Royale/src/listeners/EntityTransformListener.java
@@ -1,17 +1,17 @@
 package listeners;
 
 import org.bukkit.entity.Villager;
+import org.bukkit.entity.Witch;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTransformEvent;
 import org.bukkit.event.entity.EntityTransformEvent.TransformReason;
 
-
 public class EntityTransformListener implements Listener {
 
     @EventHandler
     public void onEntityTransformEvent(EntityTransformEvent event) {
-        if(event.getTransformedEntity() instanceof Villager && event.getTransformReason().equals(TransformReason.LIGHTNING)) {
+        if(event.getTransformedEntity() instanceof Witch && event.getTransformReason().equals(TransformReason.LIGHTNING)) {
             event.setCancelled(true);
         }
     }
